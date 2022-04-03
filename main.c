9af0c48d12f493401d517fd56cae072bb64170a7
rphii <raphael.seitz18@gmail.com>
2022-04-03 18:45:53 +0200

    remove all spaces that I could remove

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-int main(int c, char **v)
+#include<stdio.h>
+int main(int c,char **v)
 {
-    for(char *h = *v; *h; h++) *v = *h == F ? h + 1 : ((*h = *h == '.' ? 0 : *h) & 0) + *v;
+    for(char *h=*v;*h;h++)*v=*h==F?h+1:((*h=*h=='.'?0:*h)&0)+*v;
     puts(*v);
     return 0;
 }
